434c673bdb1c551a1682640d4e962fcdb30be77c
Sheetalpandey12 <sheetalpandey555@gmail.com>
2022-10-19 23:10:47 +0530

    first problem of missing array solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,130 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+// let player =
+// {
+//     "name": "manish",
+//     "dob": "1/1/1995",
+//     "gender": "male",
+//     "city": "jalandhar",
+//     "sports": [
+//     "swimming"
+//     ]
+//     }
+//     ,
+//     {
+//         name : "manish",
+//         "dob": "1/1/1995",
+//         "gender": "male",
+//         "city": "jalandhar",
+//         "sports": [
+//         "swimming"
+//         ]
+//         }
 
+// problem 1
+// -write an api which gives the missing number in an array of integers starting 
+// from 1….e.g [1,2,3,5,6,7] answer = 4 is missing
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
+     app.get("/solution", function (req, res) {
+         let arr= [1,2,3,5,6,7]
+         expectedNumber = arr.length+1;
+         totalSum = expectedNumber*(expectedNumber+1)/2;
+          console.log(totalSum);
+         let sum = 0;
+         for(let i=0;i,arr.length;i++){
+            sum = sum +arr[i]
+         }
+         console.log(sum)
+///LOGIC WILL GO HERE
+res.send( {" missing number is" + totalSum - sum } );
+});
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+// router.get('/students/:name', function(req, res) {
+//     let studentName = req.params.name
+//     console.log(studentName)
+//     res.send(studentName)
+// })
+
+// router.get("/random" , function(req, res) {
+//     res.send("hi there")
+// })
+
+
+// router.get("/test-api" , function(req, res) {
+//     res.send("hi FunctionUp")
+// })
+
+
+// router.get("/test-api-2" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API")
+// })
+
+
+// router.get("/test-api-3" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
+// })
+
+
+// router.get("/test-api-4" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+// })
+
+
+
+// router.get("/test-api-5" , function(req, res) {
+//     res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+// })
+
+// router.get("/test-api-6" , function(req, res) {
+//     res.send({a:56, b: 45})
+// })
+
+// router.post("/test-post", function(req, res) {
+//     res.send([ 23, 45 , 6])
+// })
+
+
+// router.post("/test-post-2", function(req, res) {
+//     res.send(  { msg: "hi" , status: true }  )
+// })
+
+// router.post("/test-post-3", function(req, res) {
+//     // let id = req.body.user
+//     // let pwd= req.body.password
+
+//     // console.log( id , pwd)
+
+//     console.log( req.body )
+
+//     res.send(  { msg: "hi" , status: true }  )
+// })
+
+
+
+// router.post("/test-post-4", function(req, res) {
+//     let arr= [ 12, "functionup"]
+//     let ele= req.body.element
+//     arr.push(ele)
+//     res.send(  { msg: arr , status: true }  )
+// })
 
 module.exports = router;
