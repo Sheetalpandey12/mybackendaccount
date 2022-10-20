d0c66b07772606ca66c2adc65f745787a157b840
Sheetalpandey12 <sheetalpandey555@gmail.com>
2022-10-21 00:03:34 +0530

    i have done with my assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,53 +2,46 @@ const { json } = require('express');
 const express = require('express');
 const router = express.Router();
 
+let persons= [
+  {
+  name: "PK",
+  age: 10,
+  votingStatus: false
+},
+{
+  name: "SK",
+  age: 20,
+  votingStatus: false
+},
+{
+  name: "AA",
+  age: 70,
+  votingStatus: false
+},
+{
+  name: "SC",
+  age: 5,
+  votingStatus: false
+},
+{
+  name: "HO",
+  age: 40,
+  votingStatus: false
+}
+]
 
 
-let players =
-    [
-        {
-            "name": "manish",
-            "dob": "1/1/1995",
-            "gender": "male",
-            "city": "jalandhar",
-            "sports": [
-                "swimming"
-            ]
-        },
-        {
-            "name": "gopal",
-            "dob": "1/09/1995",
-            "gender": "male",
-            "city": "delhi",
-            "sports": [
-                "soccer"
-            ]
-        },
-        {
-            "name": "lokesh",
-            "dob": "1/1/1990",
-            "gender": "male",
-            "city": "mumbai",
-            "sports": [
-                "soccer"
-            ]
-        },
-    ]
-
-
-router.post("/player", function (req, res) {
-
-    let obj = req.body.element
-
-for(i of players){
-    if (i.name == obj.name) {
-         return res.send({ msg: "name already exits" })
-    }}
-    
-        players.push(obj)
-        console.log(obj)
-        res.send({ msg: players, status: true })
-    
+router.post('/voters',function(req,res){
+  let input =req.query.votingage
+  let voter=[]
+  for(let i=0;i<persons.length;i++){
+    if(persons[i].age>input){
+        persons[i].votingStatus=true
+        voter.push(persons[i])
+    }
+  }
+  
+  res.send(voter)
 })
 
 
