a8277f1247b4fa1690b7fc94710b423f74708a07
Sheetalpandey12 <sheetalpandey555@gmail.com>
2022-10-20 12:10:05 +0530

    completed missing array assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,46 @@ const express = require('express');
 const router = express.Router();
 
 
-assignment/missing-array
+// problem 1
+// -write an api which gives the missing number in an array of integers starting 
+// from 1….e.g [1,2,3,5,6,7] answer = 4 is missing
+
+     router.get("/solution", function (req, res) {
+         let arr= [1,2,3,5,6,7]
+         expectedNumber = arr.length+1;    // 6+1 = 7
+         totalSum = expectedNumber*(expectedNumber+1)/2; //  7*7+1/2 = 28   n(n+1)/2
+          console.log(totalSum);
+         let sum = 0;
+         for(let i=0;i<arr.length;i++){
+            sum = sum +arr[i]
+         }
+         console.log(sum)   // 1+2+3+5+6+7 =24 
+///LOGIC WILL GO HERE
+        res.send( {" missing number is": + totalSum - sum } );
+});
+
+
+
+
+// problem 2
+// -write an api which gives the missing number in an array of integers
+// starting from anywhere….e.g [33,34, 35, 37, 38]: 36 is missing
+
+router.get("/solution2", function (req, res) {
+  //logic : sum of n consecutive numbers is [ n * (first + last) / 2 ]..so get sum of all
+  //numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+  let arr= [33, 34, 35, 37, 38]
+      let n = arr.length+1;    // 5+1 = 6
+       totalSum = (arr[0]+arr[arr.length-1])*n/2   /// // no(no+1)/2  = 213
+              console.log(totalSum);
+                       let sum = 0;
+            for(let i=0;i<arr.length;i++){
+              sum = sum +arr[i]   
+           }
+            console.log(sum)   // 33+34+35+37+38 = 177
+  // ///LOGIC WILL GO HERE
+           res.send( {" missing number is": + totalSum - sum } );
+   });
 
 
 
