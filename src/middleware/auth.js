7aaa58ead770b75ba9b0c098a488ac3ad62a4d33
Sheetalpandey12 <sheetalpandey555@gmail.com>
2022-11-08 19:16:15 +0530

    done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,32 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require("jsonwebtoken");
 
-    next()
+const authenticate = function(req, res, next) {
+    try {
+        let token = req.headers["x-Auth-token"];   
+    if(! token) token= req.headers["x-auth-token"];
+    if (!token) return res.status(404).send({ status: false, msg: "token must be present" });
+  
+  let decodedToken = jwt.verify(token, "lithium-sheetal");
+  if (!decodedToken) {
+    return res.send({ status: false, msg: "token is invalid" });
+  }
+    req.loggedId = decodedToken.userId;
+  next()
+} catch (err){
+    res.send({msg:"Accessv Denied"});
 }
+};
 
 
 const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
+     let checkAuthorise = req.params.userId;
+     if(checkAuthorise !== req.loggdId){
+        return res.status(404).send({msg: "oooh!you are not valid user"});
+     }
     next()
-}
+};
+
+
+
+module.exports.authenticate = authenticate;
+module.exports.authorise = authorise;
